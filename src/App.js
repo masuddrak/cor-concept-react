df6ddc6bd86b1c2292e78237e9a8c6a06f004d83
masud rana <masudranaacb@gmail.com>
2022-03-19 22:22:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries  name=' Sakibal hassan' nationality=' Bangladesh'country=' Bangladesh'></Countries>
     </div>
   );
-}
+  };
+
+
+function Countries(props) {
+  return (
+    <div>
+      <h3>Name:{props.name}</h3>
+      <p>Country:{props.country}</p>
+      <p>Nationality:{props.nationality}</p>
+    </div>
+  );
+  };
+
+
+
+
+
+
+
 
 export default App;
