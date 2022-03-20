ba7fd29132f4c2757c582aa15a261f0dc145aec9
masud rana <masudranaacb@gmail.com>
2022-03-20 18:41:54 +0600

    event listenar add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Phone from './components/Phone/Phone';
+import Walk from './components/Walk/Walk';
 
 
 function App() {
   return (
     <div className="App">
      <Phone name='Symphone' price='87856'></Phone>
+     <Walk></Walk>
     </div>
   );
   };
---
+++ src/components/Walk/Walk.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Walk = () => {
+    const [run,setRun]=useState(0)
+    const RunBtn=()=>{
+        const newRun=run+1;
+        setRun(newRun)
+    }
+    return (
+        <div>
+            <h1>Run to first life is easy</h1>
+            <h3>Walking Count = {run}</h3>
+            <button onClick={RunBtn}>Run.......</button>
+        </div>
+    );
+};
+
+export default Walk;
