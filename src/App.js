46ced082f029f316d7c60ca25dd560e2e62919ab
masud rana <masudranaacb@gmail.com>
2022-03-20 18:06:22 +0600

    phone name add

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useState } from 'react';
+import Phone from './components/Phone/Phone';
+
 
 function App() {
   return (
     <div className="App">
-      <Countries  name=' Sakibal hassan' nationality=' Bangladesh'country=' Bangladesh'></Countries>
+     <Phone name='Symphone'></Phone>
     </div>
   );
   };
 
-
-function Countries(props) {
-  const [count,setCount]=useState(0);
-  const Counting=()=>{
-    const newCount=count+1;
-   setCount(newCount)
-  }
-
-  return (
-    <div>
-      <h3>Name:{props.name}</h3>
-      <p>Country:{props.country}</p>
-      <p>Nationality:{props.nationality}</p>
-      <p>Count:{count}</p>
-      <button onClick={Counting}>Count</button>
-    </div>
-  );
-  };
 
 
 
---
+++ src/components/Phone/Phone.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Phone = (props) => {
+    return (
+        <div>
+            <h2>Phone Name {props.name}</h2>
+        </div>
+    );
+};
+
+export default Phone;
