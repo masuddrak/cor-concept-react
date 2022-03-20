d0a2abfcb1abef6ec999813df73cb95f63675c4d
masud rana <masudranaacb@gmail.com>
2022-03-20 18:45:40 +0600

    using useEfect and updat evet

--- src/components/Walk/Walk.js
+++ src/components/Walk/Walk.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Walk = () => {
     const [run,setRun]=useState(0)
     const RunBtn=()=>{
         const newRun=run+1;
         setRun(newRun)
+        
     }
+    useEffect(()=>{
+        console.log(run)
+    },[run])
     return (
         <div>
             <h1>Run to first life is easy</h1>
